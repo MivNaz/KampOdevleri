3b0adc5a05e8e6856e60d5f0550a584c24354c67
MivNaz <nazlican.1905karatas@gmail.com>
2021-02-14 01:09:46 +0300

    14 Şubat 6. ödev.

---
+++ MyProductList/MyProductList/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProductList
+{
+    public class ProductManager : Product
+    {
+        public void Listele(List<Product> products, int secim)
+        {
+            switch (secim)
+            {
+                case 1:
+                    for (int i = 0; i < products.Count; i++)
+                    {
+                        Console.WriteLine(products[i].ProductId + "-" + products[i].ProductName + "-" + products[i].ProductPrice);
+                    }
+                    break;
+                case 2:
+                    foreach (Product item in products)
+                    {
+                        Console.WriteLine(item.ProductId + " - " + item.ProductName + " - " + item.ProductPrice);
+                    }
+                    break;
+                case 3:
+                    int say = 0;
+                    while (say < products.Count)
+                    {
+                        Console.WriteLine(products[say].ProductId + " - " + products[say].ProductName + " - " + products[say].ProductPrice);
+                        say++;
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ MyProductList/MyProductList/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyProductList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> stok = new List<Product>();
+            Product product1 = (new Product { ProductId = 1, ProductName = "Kalem", ProductPrice = 11 });
+            Product product2 = (new Product { ProductId = 2, ProductName = "Ataç ", ProductPrice = 12 });
+            Product product3 = (new Product { ProductId = 3, ProductName = "Zımba", ProductPrice = 18 });
+            Product product4 = (new Product { ProductId = 4, ProductName = "Silgi", ProductPrice = 5 });
+
+            ProductManager productManager = new ProductManager();
+            productManager.ProductId = 5;
+            productManager.ProductName = "Kalemtıraş";
+            productManager.ProductPrice = 8;
+
+            ProductManager productManager1 = new ProductManager
+            {
+                ProductId = 6,
+                ProductName = "Defter",
+                ProductPrice = 20
+            };
+
+            ProductManager productManager2 = new ProductManager
+            {
+                ProductId = 7,
+                ProductName = "Dolma  Kalem",
+                ProductPrice = 15
+            };
+
+            //Ürünlerin Listeye Eklenmesi
+            stok.Add(product1);
+            stok.Add(product2);
+            stok.Add(product3);
+            stok.Add(product4);
+            stok.Add(productManager);
+            stok.Add(productManager1);
+            stok.Add(productManager2);
+
+            Console.WriteLine("-STOKTAKİ ÜRÜNLER-");
+            Console.WriteLine("-------------------------");
+            productManager.Listele(stok, 1); //For döngüdü ile ekleme.
+            Console.WriteLine("-------------------------");
+            productManager.Listele(stok, 2); //Foreach döngüsü ile ekleme
+            Console.WriteLine("-------------------------");
+            productManager.Listele(stok, 3); //While döngüsü ile ekleme
+        }
+    }
+}
